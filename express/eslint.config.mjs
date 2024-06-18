42c58fb5b77e51bb3835f9a59846ebd695be79fb
ASIMNEUPANE <asimneupane11@gmail.com>
2024-06-19 00:31:30 +0545

    formating-eslint

--- express/eslint.config.mjs
+++ express/eslint.config.mjs
@@ -1,10 +1,20 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: { globals: globals.node },
+    //to prettifying the code using eslint
+    rules: {
+      // Indentation rule: 2 spaces
+      indent: ['error', 2],
+      // Space before function parenthesis
+      'space-before-function-paren': ['error', 'always'],
+      // No space inside parentheses
+      'space-in-parens': ['error', 'never'],
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
